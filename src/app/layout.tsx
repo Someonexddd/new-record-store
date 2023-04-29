64ebc5886b9f419858565558a79305bc4742c41f
Someonexddd <50412783+Someonexddd@users.noreply.github.com>
2023-04-29 02:40:39 +0200

    THingi works

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.scss'
+import styles from './layout.module.scss'
+import Image from 'next/image'
 
 import localFont from 'next/font/local'
 
@@ -13,7 +15,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <main className={styles.main}>
+          <header className={styles.header}>
+            <Image className={styles.navLogo} priority src={"/images/logo.png"} alt='image isnt loading' 
+              sizes='100vw' width={0} height={0} placeholder='empty'></Image>
+            <div className={styles.navbar}>
+              <p>test</p>
+            </div>
+            <div className={styles.rightSide}>
+              <p>Header</p>
+            </div>
+          </header>
+          <div className={styles.content}>
+            {children}
+          </div>
+          <footer className={styles.footer}>
+            <p>
+              Footer
+            </p>
+          </footer>
+        </main>
+      </body>
     </html>
   )
 }
